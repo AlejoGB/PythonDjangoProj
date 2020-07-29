b70adf8c2ecc89f923c8778783710090e7dca195
AlejoGB <alejo.garcia@rfindustrial.com>
2020-07-28 21:29:48 -0300

    settings email edit

--- Inmob/Inmob/settings.py
+++ Inmob/Inmob/settings.py
@@ -143,11 +143,11 @@ MESSAGE_TAGS = {
 }
 
 # Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER= 'catanzaroadmin@gmail.com'
-EMAIL_HOST_PASSWORD= 'password'
-EMAIL_USE_TLS=True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER= 'catanzaroadmin@gmail.com'
+#EMAIL_HOST_PASSWORD= 'password'
+#EMAIL_USE_TLS=True
 
 try:
     from .local_settings import *
